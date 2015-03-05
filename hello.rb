3edf607ef7c2b3a9159a58f7a33d9432f3a0e2a7
Grace Z <gz.gracez16@gmail.com>
2015-03-04 23:16:33 -0500

    h1

---
+++ hello.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+set :port, 5000
+
+get '/hello' do
+  "Hello Sinatra!"
+end
+
+get '/' do
+  "Home"
+end
+
+get '/test' do
+  test="test"
+  "Hello #{test}"
+end
+
+get '/reload' do
+  test="reload"
+  "Hello #{test}"
+end

6967aa55606c37d802817a8375718e3b0cfda1ba
Grace Z <gz.gracez16@gmail.com>
2015-04-03 17:26:06 -0400

    date well formatted, within Tasks class

--- tasks.rb
+++ tasks.rb
@@ -1,58 +1,24 @@
 require 'dm-core'
 require 'dm-migrations'
 
+configure :development do
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
+
+configure :production do
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
+end
+
 class Tasks
 	include DataMapper::Resource
 	property :id, Serial
 	property :name, String
 	property :date, Date
 	property :details, String
-end
 
-def date=date
-    super Date.strptime(date, '%m/%d/%Y')
+	def date=date
+	    super Date.strptime(date, '%m/%d/%Y')
+	end
 end
 
 DataMapper.finalize
-
-get '/tasks' do
-  @title="All Tasks"
-  @tasks = Tasks.all
-  erb :tasks
-end
-
-get '/tasks/new' do
-  @title="New Task"
-  @tasks = Tasks.new
-  erb :new_task
-end
-
-post '/tasks' do
-  tasks = Tasks.create(params[:tasks])
-  redirect to("/tasks/#{tasks.id}")
-end
-
-get '/tasks/:id' do
-  @title = Tasks.get(params[:id]).name
-  @tasks = Tasks.get(params[:id])
-  erb :show_tasks
-end
-
-get '/tasks/:id/edit' do
-  @title = "Edit " + Tasks.get(params[:id]).name
-  @tasks=Tasks.get(params[:id])
-  erb :edit_task
-end
-
-put '/tasks/:id' do
-  @title = "Update " + Tasks.get(params[:id]).name
-  tasks=Tasks.get(params[:id])
-  tasks.update(params[:tasks])
-  redirect to("/tasks/#{tasks.id}")
-end
-
-delete '/tasks/:id' do
-  @title = "Delete " + Tasks.get(params[:id]).name
-  Tasks.get(params[:id]).destroy
-  redirect to("/tasks")
-end

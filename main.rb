d3ded6a4d0e31fcbf466f21b4b41762b69e96023
Grace Z <gz.gracez16@gmail.com>
2015-04-03 17:46:29 -0400

    db config

--- main.rb
+++ main.rb
@@ -7,6 +7,14 @@ require './tasks'
 
 #get('/styles.css'){ scss :styles, :syntax => :scss, :style => :compressed }
 
+configure :development do
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
+
+configure :production do
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
+end
+
 get '/' do
   @title = "testSinatra's Home Page!"
   erb :home
